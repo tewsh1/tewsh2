c2d2e731604504601a46b5a00c79df5e1d5ce4b4
tewsh1 <tewsh1@gmail.com>
2024-09-28 07:24:08 +0000

    1

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import google.generativeai as genai
 import os
 
 model = genai.GenerativeModel("gemini-1.5-flash")
-genai.configure(api_key=api)
+api = os.getenv("MAKERSUITE")
+genai.configure(api_key="AIzaSyDAnNYU8MnVIR4pnZ0OaTWQ1RRoRCDbJfA")
 
 app = Flask(__name__)
 
-@app.route("/",methods=["Get","Post"])
+@app.route("/",methods=["GET","POST"])
 def index():
-    return(render_template('index.html'))
+    return(render_template("index.html"))
 
-@app.route("/prediction_DBS",methods=["Get","Post"])
+@app.route("/prediction_DBS",methods=["GET","POST"])
 def prediction_DBS():
-    return(render_template('prediction_DBS.html'))
+    return(render_template("prediction_DBS.html"))
 
 @app.route("/prediction_result_DBS",methods=["GET","POST"])
 def prediction_result_DBS():
@@ -23,7 +24,18 @@ def prediction_result_DBS():
 
 @app.route("/faq",methods=["GET","POST"])
 def faq():
-        return(render_template("faq.html"))
+    return(render_template("faq.html"))
+
+@app.route("/q1",methods=["GET","POST"])
+def q1():
+    r = model.generate_content("How should I diversify my investment portfolio?")
+    return(render_template("q1_reply.html",r=r))
+
+@app.route("/q2",methods=["GET","POST"])
+def q2():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("q2_reply.html",r=r))
 
 if __name__ == "__main__":
     app.run()
