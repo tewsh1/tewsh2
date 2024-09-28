a941f786e1d6e857f938e79b115fa2f42f912c84
tewsh1 <tewsh1@gmail.com>
2024-09-28 11:38:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask,render_template,request
+import google.generativeai as genai
+import os
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+genai.configure(api_key=api)
 
 app = Flask(__name__)
 
